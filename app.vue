0955e5d6a8e8ad00b734da18129ca9201c831e5c
RE <re_ainbu@liginc.co.jp>
2025-06-20 18:55:29 +0900

    fix Deployment 2 error

--- app.vue
+++ app.vue
@@ -11,5 +11,7 @@
       <a href="/contact">Contact Us</a>
       <a href="/privacy">Privacy Policy</a>
       <a href="/terms">Terms of Service</a>
+      <a href="/help">Help</a>
+    </div>
   </div>
 </template>
