d84f94a6d1feb5a3003c1535058d6635ac10380c
EC2 Default User <ec2-user@ip-172-31-3-178.ap-northeast-1.compute.internal>
2025-06-20 07:40:10 +0000

    update ec2 connection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
 })
