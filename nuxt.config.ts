38435953f9e8736c223f119410dceaf876e7a3b1
RE <re_ainbu@liginc.co.jp>
2025-06-27 12:38:47 +0900

    Revert "update ec2 connection"
    This reverts commit d84f94a6d1feb5a3003c1535058d6635ac10380c.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  devServer: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
+  devtools: { enabled: true }
 })
