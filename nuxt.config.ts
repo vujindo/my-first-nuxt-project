5857896277cac9324802396400201fa70373df37
RE <re_ainbu@liginc.co.jp>
2025-07-04 18:26:46 +0900

    set api route and proxy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  devServer: {
+    port: parseInt(process.env.PORT || '3000', 10),
+  },
+  nitro: {
+    devProxy: {
+      '/api': {
+        target: process.env.NUXT_SERVER_API_URL || 'http://localhost:4000/api',
+        changeOrigin: true,
+      }
+    }
+  },
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:3000/api',
+    },
+  },
 })
